73b99131c953e65c7092cab82956a429a7be9fc4
Chee Kian Teoh <teoh4770@mylaurier.ca>
2026-07-03 16:01:15 +0800

    feat: handle no expense scenario when remove an expense

--- app/Commands/RemoveExpenseCommand.php
+++ app/Commands/RemoveExpenseCommand.php
@@ -3,6 +3,7 @@
 namespace App\Commands;
 
 use App\Actions\RemoveExpenseAction;
+use App\Models\Expense;
 use Error;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
@@ -30,6 +31,10 @@ class RemoveExpenseCommand extends Command
     {
         $id = $this->argument('id');
 
+        if (empty($id)) {
+            $id = $this->chooseFromExpenseList();
+        }
+
         try {
             $removeExpenseAction->execute($id);
         } catch (Error $e) {
@@ -49,5 +54,26 @@ class RemoveExpenseCommand extends Command
     public function schedule(Schedule $schedule): void
     {
         // $schedule->command(static::class)->everyMinute();
+    }
+
+    private function chooseFromExpenseList(): int
+    {
+        $expenses = Expense::query()->get();
+
+        if ($expenses->isEmpty()) {
+            $this->info('There are no expenses to remove.');
+            return Command::SUCCESS;
+        }
+
+        $options = $expenses->map(function (Expense $expense) {
+            return "$expense->id - $expense->name - {$expense->created_at->format('Y-m-d')}";
+        });
+
+        $selected = $this->choice(
+            'What is the expense that you want to remove?',
+            $options->toArray()
+        );
+
+        return explode(' - ', $selected)[0];
     }
 }
--- tests/Feature/RemoveExpenseCommandTest.php
+++ tests/Feature/RemoveExpenseCommandTest.php
@@ -14,5 +14,29 @@ describe('RemoveExpenseCommand', function () {
         $this->assertModelMissing($expense);
     });
 
-    it('prompts for choosing an expense from a list if id is not provided')->skip();
+    it('prompts for choosing an expense from a list if id is not provided', function () {
+        $expense = Expense::factory()->create();
+
+        $expectedChoice = "{$expense->id} - {$expense->name} ({$expense->created_at->format('Y-m-d')})";
+
+        $this->artisan('expense:remove')
+            ->expectsQuestion('What is the expense that you want to remove?', $expectedChoice)
+            ->expectsOutput('Expense deleted successfully')
+            ->assertSuccessful();
+
+        $this->assertModelMissing($expense);
+    });
+
+    it('handle no expenses', function () {
+        $this->artisan('expense:remove')
+            ->expectsOutput('There are no expenses to remove.')
+            ->assertFailed();
+    });
+
+    it('outputs an error if expense does not exist', function () {
+       $this->artisan('expense:remove', ['id' => 9999])
+           ->expectsOutput('Expense failed to delete')
+           ->expectsOutput('There is no expense with the id of 9999')
+           ->assertFailed();
+    });
 });
